3af177b759358ef0aeb631dadf351c3838abb0c2
Satyam Raj Mahakul <mahakulsatyamraj@gmail.com>
2025-04-27 02:39:32 +0530

    initial commit

---
+++ Backend/db/db.js
@@ -0,0 +1,17 @@
+
+const mongoose = require('mongoose');
+
+function connectToDb() {
+    mongoose.connect(process.env.DB_CONNECT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to DB');
+    })
+    .catch(err => {
+        console.log('Database connection error:', err);
+    });
+}
+
+module.exports = connectToDb;
